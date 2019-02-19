3926dc5835a63b89a2f5908578f310b4e9eb0874
Abel Acosta <abelacosta1596@gmail.com>
2019-02-19 15:06:06 -0800

    Changed constructors

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,14 @@ public class Main {
 	public static void main(String[] args) {
 		// Items customer wants to order
 		ArrayList<DessertItem> items = new ArrayList<DessertItem>();
-		items.add(new Cookie(10, 499));
-		items.add(new Candy(0.5, 549));
-		items.add(new IceCream(199));
-		items.add(new Sundae(199, 50));
+		Cookie cookie = new Cookie("Chocolate Chip", 10, 499, 500);
+		Candy candy = new Candy("Snickers", 0.5, 549, 214);
+		IceCream iceCream = new IceCream("Vanilla", 199, 60);
+		Sundae sundae = new Sundae("Hot caramel", 199, 50, 100);
+		items.add(cookie);
+		items.add(candy);
+		items.add(iceCream);
+		items.add(sundae);
 		
 		// Create checkout object
 		Checkout checkout = new Checkout();
